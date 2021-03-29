519811779a353d8db2bbdb6d48ed625a8ccba3e4
aj <andrewjones237@u.boisestate.edu>
2021-03-29 13:19:42 -0600

    utilities file

---
+++ workshop_utilities.hpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+
+/*c++ basics*/
+void variable_declaration(){
+
+// variables, data types
+}
+
+void data_structures(){
+
+//arrays and STL containers
+
+}
+
+void loops(){
+
+//for and while loops
+}
+
+void conditionals(){
+
+// if/ else if / else
+
+}
+
+void file_input_output(){
+
+//write and read file
+
+}
+
+/*numerical linear algebra*/
+void gaussian_elimination(){
+
+//perform GE
+
+}
+
+void axpy(){
+
+// a*x += b*y
+
+}
+
+void gemv(){
+
+//  b = Ax
+
+}
+
+void gemm(){
+
+// C = AB
+
+}
+
+/*numerical methods*/
+
+
+
+
